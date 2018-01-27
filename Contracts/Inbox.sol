3f92d6ab95ea1a04b5f2eccf8d39e86f9ae669b2
Vikrant Kumar <vikrantkumar@vikrants-air.attlocal.net>
2018-01-26 18:50:24 -0600

    Newer Version

--- Contracts/Inbox.sol
+++ Contracts/Inbox.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.17;
 
-contract inbox {
+contract Inbox {
     string public message;
 
     function inbox(string initialMessage) public {
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,10 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+//const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
 const { interface, bytecode } = require('../compile');
 
 //beforeEach(() => {
@@ -16,11 +19,18 @@ beforeEach(async () => {
   //Use one og the accounts to deploy the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
   .deploy({ data: bytecode, arguments: ['Hi there!']})
-  .send({from: account[0], gas:'1000000'});
+  .send({from: accounts[0], gas:'1000000'});
+  inbox.setProvider(provider);
 });
 describe('Inbox', () => {
   it('deploy a contract', () => {
-  console.log(inbox);
+    assert.ok(inbox.options.address);
+  //console.log(inbox);
+});
+it('has a default message', async () => {
+  const message = await inbox.methods.message().call();
+  assert.equal(message, 'Hi there!');
+
 });
 });
 
